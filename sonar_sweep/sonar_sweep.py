7127d29a9ddcc7a38a3b2768d6958b7b08fbe02b
Jody Ribton <jody@ribton.me>
2021-12-01 10:03:36 +0000

    Day 1 Part 2 complete (sonar sweep, but with sliding windows)

--- sonar_sweep/sonar_sweep.py
+++ sonar_sweep/sonar_sweep.py
@@ -23,12 +23,43 @@ def count_depth_drops(readings: Iterator[int]) -> int:
     return depth_drops
 
 
-if __name__ == "__main__":
-    # Count depth drops in input.txt
-    with open('input.txt') as f:
-        result = count_depth_drops((int(r) for r in f))
+def count_sliding_depth_drops(readings: Iterator[int]) -> int:
+    """ Same as count_depth_drops(), but compare sums over a sliding three-value window instead of discrete values."""
 
-    print("{0} drops in depth in file input.txt".format(result))
+    # Take the first three readings as our starting point. If there aren't enough readings, return 0.
+    try:
+        window_queue = [next(readings), next(readings), next(readings)]
+        previous_sum: int = sum(window_queue)
+    except StopIteration:
+        # If we don't have enough values, return 0 (no depth increases)
+        return 0
+
+    # Iterate through remaining readings, counting the number of depth increases
+    depth_drops: int = 0
+
+    for r in readings:
+        window_queue.pop(0)
+        window_queue.append(r)
+        new_window_sum = sum(window_queue)
+
+        if new_window_sum > previous_sum:
+            depth_drops += 1
+
+        previous_sum = new_window_sum
+
+    return depth_drops
+
+
+if __name__ == "__main__":
+    # Count depth drops in input.txt (Day 1 challenge part 1)
+    with open('input.txt') as f:
+        simple_depth_drops = count_depth_drops((int(r) for r in f))
+    print("{0} drops in depth in file input.txt".format(simple_depth_drops))
+
+    # Count depth drops over a sliding three-reading window (Day 1 challenge part 2)
+    with open('input.txt') as f:
+        sliding_depth_drops = count_sliding_depth_drops((int(r) for r in f))
+    print("{0} drops over three-reading windows in file input.txt".format(sliding_depth_drops))
 
 
 def test_count_depth_drops():
@@ -47,3 +78,22 @@ def test_count_depth_drops():
     # Test a list of drops and rises
     bumpy = iter([1, 50, 100, 70, 100, 70])
     assert count_depth_drops(bumpy) == 3
+
+
+def test_count_sliding_depth_drops():
+    """ Tests for count_sliding_depth_drops() """
+
+    # An empty list of readings should return 0 drops
+    assert count_sliding_depth_drops(iter([])) == 0
+
+    # A single item should also return 0 drops
+    assert count_sliding_depth_drops(iter([9])) == 0
+
+    # 3 items should return 0 drops
+    assert count_sliding_depth_drops(iter([9, 10, 9])) == 0
+
+    # Test sliding increases (window sums are 28, 30, 32)
+    assert count_sliding_depth_drops(iter([9, 10, 9, 11, 12])) == 2
+
+    # Test sliding increases and decreases (window sums are 28, 30, 21, 24)
+    assert count_sliding_depth_drops(iter([9, 10, 9, 11, 1, 12])) == 2
